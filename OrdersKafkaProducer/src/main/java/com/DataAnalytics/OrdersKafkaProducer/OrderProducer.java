eb883fa5a55360e7d2e64bb861f955f8d234101b
RahilModi <rmodi10@gmail.com>
2017-10-30 23:05:05 -0700

    modified producer solution

--- OrdersKafkaProducer/src/main/java/com/DataAnalytics/OrdersKafkaProducer/OrderProducer.java
+++ OrdersKafkaProducer/src/main/java/com/DataAnalytics/OrdersKafkaProducer/OrderProducer.java
@@ -16,16 +16,20 @@ import java.util.Random;
 /**
  * Created by Parth on 9/14/2017.
  */
-
 public class OrderProducer {
 
+    //Type of Order Statuses
     enum STATUS{PLACED,APPROVED,INTRANSIT,SHIPPED};
 
+    //Name of the topic
     private static final String TOPIC = "orders2";
+
+    //Kafka multi broker cluster to achieve scalability
     private final static String BOOTSTRAP_SERVERS = "192.168.67.1:9092,192.168.67.129:9092,192.168.67.133:9092";
-
+    
+    //returns the instance of the KafkaProducer
     private static Producer<Long,String> createProducer(){
-
+        //initailize the properties of the kafkaproducer
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BOOTSTRAP_SERVERS);
         props.put(ProducerConfig.CLIENT_ID_CONFIG,"OrdersKafkaProducer");
@@ -34,33 +38,37 @@ public class OrderProducer {
         return new KafkaProducer<Long, String>(props);
     }
 
-    static void runProducer(final int sendMessageCount) throws Exception {
 
-        final Producer<Long,String> ordersProducer = createProducer();
-        long time = System.currentTimeMillis();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("in");
+    static void runProducer(final int sendMessageCount) throws Exception {
+        final Producer<Long,String> ordersProducer = createProducer(); //creates the kakfa-producer
+        long startingTime = System.currentTimeMillis();
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // System.out.println("in");
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         try{
             long index = 0;
             StringBuilder sb = new StringBuilder();
-            Random r = new Random();
-            r.nextInt();
+            // Random random = new Random();
+            // random.nextInt();
             while(true){
+                //StringBuilder will have string like: "2007-12-03T10:15:30,XXXXX-XXX,PLACED"
             	sb.append(dtf.format(LocalDateTime.now()) + ",");
             	sb.append("XXXXX-XXX"+",");
-            	sb.append(STATUS.values()[(int)(Math.random()*4)]);
+            	sb.append(STATUS.values()[(int)(Math.random()*4)]); //pick the value from the available list of statuses
+                //produce an instance of the record
                 final ProducerRecord<Long, String> record =
                         new ProducerRecord<>(TOPIC, index,
                                 sb.toString());
+                
+                //will be sent to Kafka
                 RecordMetadata metadata = ordersProducer.send(record).get();
-                long elapsedTime = System.currentTimeMillis() - time;
-                System.out.printf("sent record(key=%s value=%s) " +
-                                "meta(partition=%d, offset=%d) time=%d\n",
+
+                System.out.printf("sent a record of (key=%s value=%s) meta(partition=%d, offset=%d) time=%d\n",
                         record.key(), record.value(), metadata.partition(),
-                        metadata.offset(), elapsedTime);
+                        metadata.offset(), System.currentTimeMillis() - startingTime);
+
                 sb.setLength(0);
-                //index++;
+                index++;
             }
         }finally {
             ordersProducer.flush();
